013177ae5f70c1b4175c0da717e9da282436d56e
ReekyStive <reekystive@gmail.com>
2024-05-11 17:13:20 +0800

    feat(web): ✨ add server base url env variable

--- apps/web/src/server/trpc.ts
+++ apps/web/src/server/trpc.ts
@@ -1,10 +1,8 @@
 import type { AppRouter } from '@lingoodia/server/src/index.ts';
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 
+const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:4000/trpc',
-    }),
-  ],
+  links: [httpBatchLink({ url: SERVER_BASE_URL })],
 });
